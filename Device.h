a00e371227773b14d63a02713740855d736593d0
LangerKrieg <42912304+LangerKrieg@users.noreply.github.com>
2025-05-19 09:29:55 +0300

    Update Device.h

--- Device.h
+++ Device.h
@@ -1,15 +1,53 @@
-#pragma once
-#include <alsa/asoundlib.h>
+//
+// Created by kuki on 5/6/25.
+//
+
+#ifndef DEVICE_H
+#define DEVICE_H
+
 #include <string>
+#include <alsa/asoundlib.h>
 
 class Device {
+private:
+    std::string hw_;
+
+    snd_pcm_t *deviceHandle_;
+
+    snd_pcm_format_t format_;
+    snd_pcm_stream_t mode_;
+    snd_pcm_uframes_t periodSizeInFrames_;
+
+    unsigned int rate_;
+    unsigned int channels_;
+
+    void OpenDevice();
+    void ConfigureDevice();
+    void InitializeDevice();
+
+    static snd_pcm_format_t string_to_format(const std::string &str);
+    static snd_pcm_stream_t string_to_mode(const std::string &str);
+
 public:
-    Device(const std::string& name, snd_pcm_stream_t streamType);
+    // There is two types of Constructor. One is needed for receive strings.
+    Device(const std::string &hw, const std::string &format, const std::string &mode,
+        unsigned int rate = 8000, unsigned int channels = 1);
+    Device(const std::string &hw, snd_pcm_format_t format, snd_pcm_stream_t mode,
+        unsigned int rate = 8000, unsigned int channels = 1);
     ~Device();
 
-    void SetHWParams(unsigned int sampleRate, snd_pcm_format_t format, unsigned int channels);
-    snd_pcm_t* GetHandle();
+    std::string GetHw() const { return hw_; }
 
-private:
-    snd_pcm_t* handle;
+    snd_pcm_t *GetHandle() const { return deviceHandle_; }
+
+    snd_pcm_format_t GetFormat() const { return format_; }
+    snd_pcm_stream_t GetMode() const { return mode_; }
+    snd_pcm_uframes_t GetPeriodSize() const { return periodSizeInFrames_;  }
+
+    unsigned int GetRate() const { return rate_; }
+    unsigned int GetChannels() const { return channels_; }
+
 };
+
+
+#endif //DEVICE_H
