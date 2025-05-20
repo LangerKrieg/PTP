d90f8d1647027e3c6b9840b8bb45a497453a4eac
LangerKrieg <42912304+LangerKrieg@users.noreply.github.com>
2025-05-20 15:35:24 +0300

    Update Buffer.h

--- Buffer.h
+++ Buffer.h
@@ -9,37 +9,48 @@
 #include <vector>
 
 #include "Device.h"
+#include "udpstreamer.h"
 
+#include <mutex>
 
 class Buffer {
 private:
     std::vector<char> buffer_;
+
     const Device &captureDevice_;
     const Device &playbackDevice_;
 
-    unsigned int frameSize_;
+    UdpStreamer &gateway_;
+
     snd_pcm_uframes_t framesPerPeriod_;
-    unsigned int bufferSize_;
 
-    unsigned int writeInBufPosition_;
-    unsigned int writeInAlsaPosition_;
+    size_t frameSize_;
+    size_t bufferSize_;
+    size_t fillFrames_;
 
-    unsigned int fillFrames_;
+    size_t writeInBufPosition_;
+    size_t writeInAlsaPosition_;
 
     static int format_to_bytes(snd_pcm_format_t format);
+
+    std::mutex bufferMutex_;
+    size_t writeFromUdpPosition_;
 
     void ErrorHandler(int error, snd_pcm_stream_t mode) const;
     static void RecoverBuffer(const Device &device, int error);
 
 public:
-    explicit Buffer(const Device &capture_device, const Device &playback_device);
+    explicit Buffer(const Device &capture_device, const Device &playback_device,
+        UdpStreamer& udp);
 
     snd_pcm_sframes_t ReadFromDevice();
     snd_pcm_sframes_t WriteToDevice();
 
+    snd_pcm_sframes_t ReceiveFromUDP();
+    snd_pcm_sframes_t SendToUDP();
+
     unsigned int GetFillFrames() const { return fillFrames_; }
     unsigned int GetFramesPerPeriod() const { return framesPerPeriod_; }
-    const std::vector<char>& getRawBuffer() const { return buffer_; }
 };
 
 
